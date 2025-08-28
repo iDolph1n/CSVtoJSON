4c9cb83efd89fb6eefca39ff13b8c71d5b8a4a84
Vitaly Mikhailov <vitaly.mikhaylov94@yandex.ru>
2025-08-28 14:00:48 +0500

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        if (list != null) {
+            String json = listToJson(list);
+            writeString(json, "data.json");
+        } else {
+            logger.warning("Список сотрудников пуст. JSON файл не будет создан.");
+        }
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (FileReader reader = new FileReader(fileName)) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+
+            return csv.parse();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Ошибка чтения CSV файла: " + fileName, e);
+            return null;
+        }
+    }
+
+    public static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
+            file.write(json);
+            file.flush();
+            logger.info("JSON файл успешно создан: " + fileName);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Ошибка записи JSON файла: " + fileName, e);
+        }
+    }
+}
