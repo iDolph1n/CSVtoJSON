728f42e288399a811f043d4c0afd550b196d0e8e
Vitaly Mikhailov <vitaly.mikhaylov94@yandex.ru>
2025-08-28 14:23:58 +0500

    XML parser added

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,10 +7,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,15 +24,22 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        // CSV → JSON
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
+        String csvFileName = "data.csv";
 
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        if (list != null) {
-            String json = listToJson(list);
-            writeString(json, "data.json");
-        } else {
-            logger.warning("Список сотрудников пуст. JSON файл не будет создан.");
+        List<Employee> csvList = parseCSV(columnMapping, csvFileName);
+        if (csvList != null) {
+            String jsonFromCsv = listToJson(csvList);
+            writeString(jsonFromCsv, "data.json");
+        }
+
+        // XML → JSON
+        String xmlFileName = "data.xml";
+        List<Employee> xmlList = parseXML(xmlFileName);
+        if (xmlList != null) {
+            String jsonFromXml = listToJson(xmlList);
+            writeString(jsonFromXml, "data2.json");
         }
     }
 
@@ -41,11 +53,47 @@ public class Main {
                     .withMappingStrategy(strategy)
                     .build();
 
-            return csv.parse();
+            List<Employee> list = csv.parse();
+            logger.info("CSV файл успешно прочитан: " + fileName);
+            return list;
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Ошибка чтения CSV файла: " + fileName, e);
             return null;
         }
+    }
+
+    public static List<Employee> parseXML(String fileName) {
+        List<Employee> list = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+            doc.getDocumentElement().normalize();
+
+            Node root = doc.getDocumentElement(); // <staff>
+            NodeList nodeList = root.getChildNodes();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elem = (Element) node;
+
+                    long id = Long.parseLong(elem.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = elem.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = elem.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = elem.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(elem.getElementsByTagName("age").item(0).getTextContent());
+
+                    Employee emp = new Employee(id, firstName, lastName, country, age);
+                    list.add(emp);
+                }
+            }
+            logger.info("XML файл успешно прочитан: " + fileName);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Ошибка при парсинге XML файла: " + fileName, e);
+        }
+        return list;
     }
 
     public static String listToJson(List<Employee> list) {
